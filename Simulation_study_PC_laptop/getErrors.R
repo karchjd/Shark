26383a206f0ea1453855a988ae38bbb027aac7ba
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2019-04-10 11:40:20 +0200

    faster more elegant getError

--- Simulation_study_PC_laptop/getErrors.R
+++ Simulation_study_PC_laptop/getErrors.R
@@ -1,14 +1,5 @@
-welchError <- permError <- typeI <- rep(NA,nrow(Design))
-for (i in 1:nrow(Design)){
-  resultsDesignRowI <- Design[i,]
-  isEqualtoDesign <- rep(FALSE,nrow(ResultsSimAll))
-  cat(sprintf('Design Row %i\n',i))
-  for (j in 1:nrow(ResultsSimAll)){
-    isEqualtoDesign[j] <- all(ResultsSimAll[j,1:ncol(Design)]==resultsDesignRowI)
-  }
-  resultsForDesignI <- ResultsSimAll[isEqualtoDesign,]
-  welchError[i] <- mean(resultsForDesignI$Method1)
-  permError[i] <- mean(resultsForDesignI$Method2)
-  typeI[i] <- resultsDesignRowI$es==0
-}
-results <- cbind(Design,welchError,permError,typeI)
+library(dplyr)
+groups <- group_by_at(ResultsSimAll, names(Design))
+results <- as.data.frame(summarise(groups, mean(Method1), mean(Method2)))
+results$typeI <- results$es==0
+results <- merge(Design,results,by=names(Design),sort=FALSE)
